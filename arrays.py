a4e01647cb9ad3557bed7ad5ce4f7da309dc2841
Jason <j450nx@gmail.com>
2022-11-23 09:44:50 -0500

    intro to data structures

---
+++ arrays.py
@@ -0,0 +1,15 @@
+new_list = [1, 2, 3]
+
+if 1 in new_list: print(True)
+
+new_list.insert(0, 4)
+print(new_list)
+
+new_list.append(5)
+print(new_list)
+
+new_list.extend([8, 8])
+print(new_list)
+
+new_list.remove(2   )
+print(new_list)
